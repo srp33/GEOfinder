c558cb04aa2b15a941d851089237a44e51c2a574
abbymuir47 <amuir4@byu.edu>
2024-05-15 14:17:48 -0600

    add dictionary functionality with JSON, add more checks on inputs, more bulma details

--- WebApp.py
+++ WebApp.py
@@ -1,7 +1,7 @@
 # Final Copy
 import cherrypy
 import re 
-#import simpleDictionary
+import json
 
 class WebApp:
 
@@ -16,6 +16,11 @@ class WebApp:
 
     #Internal:
 
+    def import_dict():
+        with open("myDict.json", "r") as read_file:
+            my_dict = json.loads(read_file.read())
+        return my_dict
+
     def top_half_html(self, ids = ""):
         return f"""
         <html>
@@ -28,9 +33,9 @@ class WebApp:
         <h1 class="mt-3 subtitle is-3 has-text-centered is-family-sans-serif">Enter GEO Accession IDs:</h1>
         <form action="/query" method="post">
             <textarea
-                class="content is-medium textarea has-fixed-size textarea is-hovered textarea is-danger"
+                class="content is-medium textarea has-fixed-size textarea is-hovered textarea is-info"
                 name="ids" value = "{ids}" placeholder="Enter IDs (ie. GSE123, GSE456)" rows="10"></textarea>
-            <button class="button is-danger" type="submit">Submit</button>
+            <button class="button is-info" type="submit">Submit</button>
         </form>
             
         """
@@ -42,13 +47,10 @@ class WebApp:
     #<input type="text" name="ids" value = "{ids}" placeholder="Enter IDs (ie. GSE123, GSE456)">
     def bottom_half_html(self, ids):
         return f"""
-        <h1 class="py-6 mt-3 subtitle is-3 has-text-centered is-family-sans-serif">Relevent Studies:</h1>
+        <h1 class="py-4 mt-3 subtitle is-3 has-text-centered is-family-sans-serif">Relevant Studies:</h1>
         <div class="columns is-centered">
             <div class="columns is-three-quarters">
                 <table class="table is-size-medium" id="myTable" border="1">
-                    <tr>
-                        <th class=>GSE ID</th>
-                    </tr>
                     {self.generate_table_rows(ids)}
                 </table>
             </div>
@@ -60,17 +62,43 @@ class WebApp:
     def generate_table_rows(self, ids):
         if (ids == ""):
             return ""
-        rows = ""
-        ids = ids.strip().upper()
-        invalid = []
-        for id in re.split(r"\n|,",ids):
-            if not re.search(r"GSE\d+",id):
-                invalid.append(id)
-            else:
-                rows += f"<tr><td>{id}</td></tr>"
-        if invalid:
-            return f"<h3>Sorry, the following IDs you entered were invalid: {invalid}</h3>"
-        return rows
+        
+        id_lst = re.split(r"\n|,",ids.strip())
+        bad_format_ids = []
+        not_found_ids = []
+        valid_ids = []
+        
+        my_dict = WebApp.import_dict()
+        # print(f"keys: {my_dict.keys()}")
+
+        for id in id_lst:
+            id = id.strip().upper()
+
+            if not re.search(r"GSE\d+",id.strip().upper()):
+                bad_format_ids.append(id)
+            elif id not in my_dict.keys():
+                not_found_ids.append(id)
+            else: 
+                valid_ids.append(id)
+
+        #test values: gse001, gse002, gse789, gse990, jkf292, fif404
+        if bad_format_ids or not_found_ids:    
+            error_message = ""
+
+            if bad_format_ids:
+                error_message += f"<tr><td>Sorry, the following IDs you entered were formatted incorrectly: {", ".join(bad_format_ids)}</td></tr>"
+            if not_found_ids:
+                error_message +=f"<tr><td>The following IDs you entered were not found in our database: {", ".join(not_found_ids)}</td></tr>"
+            if valid_ids:
+                error_message +=f"<tr><td>The following IDs you entered were valid: {", ".join(valid_ids)}</td></tr>"
+            
+            return error_message
+        
+        else:
+            rows = "<tr> <th>GSE ID</th> <th>Description</th> <th>Platform</th> <th>Samples</th> </tr>"
+            for id in valid_ids:
+                rows += f"<tr> <td>{id}</td>  <td>{my_dict[id]['Description']}</td>  <td>{my_dict[id]['Platform']}</td>  <td>{my_dict[id]['Samples']}</td> </tr>"
+            return rows
     
     
 
