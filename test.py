d7a60ce2d7a835f0fb8af868d34c8398f43264f9
AnnaTubbs <annatubbs@outlook.com>
2024-05-16 15:14:21 -0600

    delete extra test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import json 
-
-
-if __name__ == '__main__':
-    with open("myDict.json") as readFile:
-        # converts json into dictionary
-        myDict = json.loads(readFile.read())
-        print(myDict)
--- test02.py
+++
@@ -1,9 +0,0 @@
-import test01
-
-def main():
-    test01.print_input("This works, yay!!!")
-
-if __name__ == '__main__':
-    with open("myDict.json") as readFile:
-        # converts json into dictionary
-        main()
