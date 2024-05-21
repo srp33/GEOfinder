82d9525531fa6fa766d24a20dc603fc54d5c9db7
abbymuir47 <amuir4@byu.edu>
2024-05-21 15:11:13 -0600

    Add files via upload
    plays with javascript and works to gray out the submit button, etc

---
+++ tryingThings.py
@@ -0,0 +1,44 @@
+import cherrypy
+
+class test:
+
+    # <button onclick="toggleTable()">Generate</button>
+    
+    @cherrypy.expose
+    def index(self):
+        return ''' <html> 
+        <head></head>
+        <body>
+
+        <textarea id="user_input" class="content is-medium textarea has-fixed-size textarea is-hovered textarea is-info"
+        name="ids" placeholder="Enter IDs (ie. GSE123, GSE456)" rows="10" oninput="check_input()"></textarea>
+        <button id="submit_button" class="button is-info" type="submit" onclick="submit_form()" disabled>Submit</button>
+
+        <script>
+        <!-- comment :) -->
+        function check_input() {
+                var input = document.getElementById('user_input').value;
+                var button = document.getElementById('submit_button');
+                button.disabled = input === '';
+        } 
+        function submit_form() {
+            // Disable the submit button
+            var button = document.getElementById('submit_button');
+            var textarea = document.getElementById('user_input');
+            button.disabled = true;
+            textarea.value = "";
+            textarea.placeholder = "loading results..."
+            // call other function that's supposed to be called 
+        }
+        </script>
+        </body>
+        </head>
+'''
+    
+    # process input (whether or not name was input)
+    @cherrypy.expose
+    def getName(self, name=""):
+        pass
+
+if __name__ == '__main__':
+    cherrypy.quickstart(test())
